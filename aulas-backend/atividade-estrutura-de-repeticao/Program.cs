ee9cced0b55c62dcfdbf1bd0082bcb540caf019d
Eduardo Felipe <48114302801@info.local>
2023-04-05 15:46:31 -0300

    Realizado exercicio estrutura de repeticao

---
+++ aulas-backend/atividade-estrutura-de-repeticao/Program.cs
@@ -0,0 +1,78 @@
+﻿//Faça um programa que leia e valide as seguintes informações:
+// 1. Nome: diferente de vazio;
+// 2. Idade: entre 0 e 100 anos;
+// 3. Salário: maior que zero;
+// 4. Estado Civil: 's'(solteiro(a)), 'c'(casado(a)), 'v'(viúvo(a)), 'd'(divorciado(a));
+
+bool nomeCerto = true;
+
+do
+{
+
+    Console.WriteLine($"Informe seu nome *Campo obrigatório*");
+    string nome = Console.ReadLine()!;
+
+    if (nome == "")
+    {
+        nomeCerto = true;
+    }
+
+    else
+    nomeCerto = false;
+
+} while (nomeCerto);
+
+bool idadeCerta = true;
+
+do
+{
+
+    Console.WriteLine($"Informe sua idade: ");
+    int idade = int.Parse(Console.ReadLine()!);
+
+    if (idade > 0 && idade < 100) 
+    {
+        idadeCerta = false;
+    }
+    else
+    nomeCerto = true;
+
+} while (idadeCerta);
+
+bool salarioCerto = true;
+
+do
+{
+
+    Console.WriteLine($"Informe seu salário: ");
+    float salario = float.Parse(Console.ReadLine()!);
+
+    if (salario > 0)
+    {
+        salarioCerto = false;
+    }
+    else
+    salarioCerto = true;
+    
+} while (salarioCerto);
+
+bool estadoCivil = true;
+
+do 
+{
+
+    Console.WriteLine($"Informe seu estado civil *Utilize 's' para solteiro 'c' para casado, 'v' para viúvo e 'd' para divorciado* ");
+    char ec = char.Parse(Console.ReadLine()!);
+
+    if (ec != 's' && ec != 'c' && ec != 'd' && ec != 'v')
+    {
+    estadoCivil = true;
+    }
+    else 
+    estadoCivil = false;
+
+} while (estadoCivil);
+
+Console.WriteLine($"Cadastro realizado com sucesso !");
+
+
--- aulas-backend/estruturas-repeticao/Program.cs
+++ aulas-backend/estruturas-repeticao/Program.cs
@@ -6,3 +6,61 @@
 //     Console.WriteLine(x); // Imprima o valor de x
 //     x+= 2; // Depois incrementa +1 ao valor de x
 // }
+
+
+
+
+// bool idadeCerta = false;
+
+// while (!(idadeCerta == true))
+// {
+//     Console.WriteLine($"Qual a idade do CR7 ?");
+//     int idade = int.Parse(Console.ReadLine());
+
+//     if (idade == 38)
+//     {
+//         idadeCerta = true;
+//     }    
+// }
+
+
+
+
+
+
+// Console.WriteLine($"Informe o nome do usuário: ");
+// string nome = Console.ReadLine();
+
+// Console.WriteLine($"Informe a senha com 6 caracteres: ");
+// string senha = Console.ReadLine();
+
+// while (senha.Length != 6)
+// {
+//     Console.WriteLine($"Infome a senha novamente, tem que ter 6 caracteres");
+//     senha = Console.ReadLine();   
+// }
+// Console.WriteLine($"Usuário e senha recebidos com sucesso!");
+
+
+
+
+
+
+
+// bool idadeCerta = false;
+// do
+// {
+//     Console.WriteLine($"Qual a idade do CR7 ?");
+//     int idade = int.Parse(Console.ReadLine());
+
+//     if(idade == 38)
+//     {
+//         Console.WriteLine($"Acertou!!");
+//         idadeCerta = true;        
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Errou feio, tente novamente!");        
+//     }
+    
+// } while (idadeCerta == false);
