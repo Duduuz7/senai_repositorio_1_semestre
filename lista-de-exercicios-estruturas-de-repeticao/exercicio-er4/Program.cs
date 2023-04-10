e78498d498c02d77e54e142122d86dbba3e5d34c
Eduardo Felipe <48114302801@info.local>
2023-04-10 17:27:44 -0300

    Terminado exercicios e iniciada aula de array

--- lista-de-exercicios-estruturas-de-repeticao/exercicio-er4/Program.cs
+++ lista-de-exercicios-estruturas-de-repeticao/exercicio-er4/Program.cs
@@ -5,40 +5,98 @@
 // C.O número de mulheres que responderam SIM;
 // D.A porcentagem de homens que responderam NÃO entre todos os homens analisados.
 
-//perguntar se e homem ou mulher, perguntar se gostou do produto,
 
-int pessoasS = 0;
-int pessoasN = 0;
-int femS = 0;
-int mascN = 0;
+//saber o sexo do intrevistado
+//saber se essa pessoa gostou do produto
 
-for (int contador = 0; contador < 10 ; contador++)
+//saber o numero de pessoas que responderam sim
+//saber o numero de pessoas que responderam nao
+
+//saber o numero de mulheres que responderam
+//saber a quantidade de mulheres que responderam sim
+
+//saber o numero de homens que responderam
+//saber o numero de homens que responderam nao
+
+//saber a porcentagem de homens que responderam nao ----- exibido
+
+
+//declarar as variaveis
+
+char sexo; 
+char gostouDoProduto;
+
+int totalSim = 0;
+int totalNao = 0;
+
+int totalMulher = 0;
+int totalMulherSim = 0;
+
+int totalHomemNao = 0;
+int totalHomem = 0;
+
+float porcentagemHomemNao = 0;
+
+//primeiro valor inicializador, segundo condicional, ate onde vai, e terceiro iterador
+for (var i = 1; i < 10; i++)
 {
-    Console.WriteLine($"Qual seu sexo ? Utilize 'm' para masculino e 's'.");
-    char sexo = char.Parse(Console.ReadLine()!);
+    //entrada de dados
 
-    Console.WriteLine($"Voce gostou do produto ? Utilize 's' para sim e 'n' para não.");
-    char gosto = char.Parse(Console.ReadLine()!);
+    Console.WriteLine(@$"
+    Informe o sexo da pessoa: 
+    (m) - Masculino
+    (f) - Feminino
+    ");
+    
+    sexo = char.Parse(Console.ReadLine()!.ToLower());
+    
+    //processamento
 
-    switch (gosto)
+    if (sexo == 'f')
     {
-        case 's':
-            pessoasS ++;
+        totalMulher++;
+    }
+    else
+    {
+        totalHomem++;
+    }
 
-            if (sexo == 'f')
-            {
-                femS ++;
-            }
-            break;
-        
-        case 'n':
-            pessoasN ++;
+    Console.WriteLine(@$"
+    A pessoa gostou do lançamento ?
+    (s) - Sim
+    (n) - Não
+    ");
 
-            if (sexo == 'm')
-            {
-                mascN ++;
-            }
-        break;
+    gostouDoProduto = char.Parse(Console.ReadLine()!.ToLower());
+    
+    if (gostouDoProduto == 's')
+    {
+        totalSim++;
+
+        if (sexo == 'f')
+        {
+            totalMulherSim++;
+        }
+    }
+
+    else
+    {
+        totalNao++;
+
+        if (sexo == 'm')
+        {
+            totalHomemNao++;
+        }
+
     }
 }
 
+porcentagemHomemNao = ((float)totalHomemNao / (float)totalHomem)*100;
+
+
+Console.WriteLine($"O total de pessoas que responderam sim foi de : {totalSim}");
+Console.WriteLine($"O total de pessoas que responderam não foi de : {totalNao}");
+
+Console.WriteLine($"O total de mulheres que responderam sim foi de : {totalMulherSim}");
+
+Console.WriteLine($"A porcentagem de homens que responderam que não gostaram no produto é de: {porcentagemHomemNao} %");
---
+++ lista-de-exercicios-estruturas-de-repeticao/exercicio-er5/Program.cs
@@ -0,0 +1,81 @@
+﻿//5. Faça um programa que receba a idade, o peso e o sexo de 10 pessoas. Calcule e imprima:
+
+// A.Total de Homens;      
+// B.Total de Mulheres;      
+// C.Média de idade dos Homens;      
+// D.Média de idade das mulheres.
+
+// char sexo;
+
+// float idade = 0;
+// float peso = 0;
+
+int totalHomem = 0;
+int totalMulher = 0;
+
+float mediaHomem = 0;
+float mediaMulher = 0;
+
+
+for (int cont = 1; cont <= 3; cont++)
+{
+    Console.WriteLine(@$"
+Qual seu sexo ?
+(m) - Masculino
+(f) - Feminino
+");
+
+    char sexo = char.Parse(Console.ReadLine()!);
+    while (sexo != 'f' && sexo != 'm')
+    {
+        Console.WriteLine($"Erro. Digite novamente: ");
+        sexo = char.Parse(Console.ReadLine()!);
+    }
+
+    if (sexo == 'm')
+    {
+        totalHomem++;
+    }
+    else
+    {
+        totalMulher++;
+    }
+
+
+
+    Console.WriteLine($"Qual sua idade ?");
+    int idade = int.Parse(Console.ReadLine()!);
+
+    while (idade < 0)
+    {
+        Console.WriteLine($"Erro. Digite novamente: ");
+        idade = int.Parse(Console.ReadLine()!);
+    }
+
+    if (sexo == 'm')
+    {
+        mediaHomem += idade;
+    }
+    else
+    {
+        mediaMulher += idade;
+    }
+
+
+
+
+    Console.WriteLine($"Qual seu peso ?");
+    float peso = float.Parse(Console.ReadLine()!);
+
+    while (peso < 0)
+    {
+        Console.WriteLine($"Erro. Digite novamente: ");
+        peso = float.Parse(Console.ReadLine()!);
+    }
+
+}
+
+Console.WriteLine($"Total de homens é igual a: {totalHomem}");
+Console.WriteLine($"Total de mulheres é igual a: {totalMulher}");
+Console.WriteLine($"A média de idade das mulheres é igual a: {mediaMulher / totalMulher}");
+Console.WriteLine($"A média de idade dos homens é igual a: {mediaHomem / totalHomem}");
