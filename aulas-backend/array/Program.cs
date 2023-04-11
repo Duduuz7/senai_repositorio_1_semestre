47926b8332abeeb9c5d31eb49aa9c26c94112873
Eduardo Felipe <48114302801@info.local>
2023-04-11 17:26:39 -0300

    Feito exercicios de fixação sobre array

--- aulas-backend/array/Program.cs
+++ aulas-backend/array/Program.cs
@@ -1,1 +1,15 @@
-﻿ 
+﻿string[]carros = new string[3];
+
+for (int i = 0; i < 3; i++)
+{
+    //bloco de código a se executado
+    Console.WriteLine($"Digite o nome do {i + 1}º carro: ");
+    carros[i] = Console.ReadLine()!;
+    
+}
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine($"O {i + 1}º carro é: {carros[i]}");
+    
+}
---
+++ exercicio-array/Program.cs
@@ -0,0 +1,19 @@
+﻿// // // Criar um programa em c# que receba 5 números inteiros e ao final exiba o seu dobro
+
+// // Você deve utilizar a estrutura array com tamanho 5 para o armazenamento dos números, a estrutura for para a leitura dos números e a estrutura foreach para exibí-los
+
+int[]numeros = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    
+    Console.WriteLine($"Digite o {i + 1}º número: ");
+    numeros[i] = int.Parse(Console.ReadLine()!);
+    
+}
+
+foreach (var item in numeros)
+{
+    Console.WriteLine($"O dobro de {item} é: {item *2}");
+    
+}
