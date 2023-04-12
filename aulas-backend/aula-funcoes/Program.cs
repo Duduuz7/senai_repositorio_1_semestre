631eaf6467aa29ef7f8adc2d339d88c16c49d4ed
Eduardo Felipe <48114302801@info.local>
2023-04-12 17:26:42 -0300

    aula sobre funções e exercicios

---
+++ aulas-backend/aula-funcoes/Program.cs
@@ -0,0 +1,96 @@
+﻿// BomDia();
+// BoaTarde();
+
+// Console.WriteLine(BoaNoite());
+
+// string batatinha = BoaNoite();
+// Console.WriteLine(batatinha);
+
+
+
+// static void BomDia(){
+//     Console.WriteLine($"Bom dia!");
+// }
+
+// static void BoaTarde(){
+//     Console.WriteLine($"Boa Tarde!");
+// }
+
+// static string BoaNoite(){
+//     return "Boa Noite!";    
+// } 
+
+
+
+
+// Criar um método para somar dois números
+
+
+
+// static float Soma(float n1, float n2){
+//     float r = n1 + n2;
+//     return r;
+// }
+
+// float batatinha = Soma(16, 123);
+
+// Console.WriteLine($"Resultado: {batatinha}");
+
+
+
+// criar método para multiplicar 2 números
+
+
+static float mult(float n1, float n2){
+
+   return (n1 * n2);
+}
+
+Console.WriteLine($"Digite o primeiro número: ");
+float n1 = float.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Digite o segundo número: ");
+float n2 = float.Parse(Console.ReadLine()!);
+
+Console.WriteLine(@$"
+O resultado da multiplicação é: {mult(n1,n2)}
+");
+
+
+
+
+
+// criar método para dividir 2 números
+
+// static float div(float n1, float n2){
+
+//    return (n1 * n2);
+// }
+
+// Console.WriteLine($"Digite o primeiro número: ");
+// float n1 = float.Parse(Console.ReadLine()!);
+
+// Console.WriteLine($"Digite o segundo número: ");
+// float n2 = float.Parse(Console.ReadLine()!);
+
+// Console.WriteLine(@$"
+// O resultado da multiplicação é: {div(n1,n2)}
+// ");
+
+
+// criar método para subtrair 2 números
+
+// static float sub(float n1, float n2){
+
+//    return (n1 * n2);
+// }
+
+// Console.WriteLine($"Digite o primeiro número: ");
+// float n1 = float.Parse(Console.ReadLine()!);
+
+// Console.WriteLine($"Digite o segundo número: ");
+// float n2 = float.Parse(Console.ReadLine()!);
+
+// Console.WriteLine(@$"
+// O resultado da multiplicação é: {sub(n1,n2)}
+// ");
---
+++ aulas-backend/exercicio-de-fixacao-array/Program.cs
@@ -0,0 +1,67 @@
+﻿// EXERCICIO NOME E IDADE
+
+
+
+
+// int[] idade = new int[5];
+// string[] nome = new string[5];
+
+// for (int i = 0; i < 5; i++)
+// {
+
+//     Console.WriteLine($"Digite o {i + 1}º nome: ");
+//     nome[i] = Console.ReadLine()!;
+
+//     Console.WriteLine($"Digite a {i + 1}º idade: ");
+//     idade[i] = int.Parse(Console.ReadLine()!);
+
+// }
+
+// for (int i = 0; i < 5; i++)
+// {
+//     Console.ForegroundColor = ConsoleColor.Blue;
+//     Console.WriteLine($"{i + 1}) nome: {nome[i]}");
+//     Console.ResetColor();
+//     Console.ForegroundColor = ConsoleColor.Green;
+//     Console.WriteLine($"idade: {idade[i]} anos");
+//     Console.ResetColor();
+//
+//     Console.WriteLine();
+// }
+
+
+
+
+
+// EXERCICIO PAR E IMPAR
+
+
+
+
+int[] numero = new int[6];
+
+for (int i = 0; i < 6; i++)
+{
+
+    Console.WriteLine($"Digite o número desejado: ");
+    numero[i] = int.Parse(Console.ReadLine()!);
+}
+
+int numeroPar = 0;
+int numeroImpar = 0;
+
+for (int i = 0; i < 6; i++)
+{
+    if (numero[i] % 2 == 0)
+    {
+        numeroPar++;
+    }
+    else
+    {
+        numeroImpar++;
+    }
+}
+
+Console.WriteLine($"{numeroPar} números são pares, e {numeroImpar} números são impares");
+
+ 
