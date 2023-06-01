e918d517f97c7c21fd2523704b49e7a473eaed01
Eduardo Felipe <48114302801@info.local>
2023-06-01 17:22:21 -0300

    Continuação projeto gamer backend

--- aulas-backend-e-razormvc/projeto-gamer-backendmvc/Controllers/EquipeController.cs
+++ aulas-backend-e-razormvc/projeto-gamer-backendmvc/Controllers/EquipeController.cs
@@ -55,9 +55,9 @@ namespace projeto_gamer_backendmvc.Controllers
 
                 var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
 
-                if (Directory.Exists(folder))
+                if (!Directory.Exists(folder))
                 {
-                    Directory.CreateDirectory(folder)
+                    Directory.CreateDirectory(folder);
                 }
 
                 var path = Path.Combine(folder, file.FileName);
@@ -83,9 +83,80 @@ namespace projeto_gamer_backendmvc.Controllers
             //Salva alteracoes
             c.SaveChanges();
 
+
             return LocalRedirect("~/Equipe/Listar");
         }
 
+        [Route("Excluir/{id}")]
+        public IActionResult Excluir(int id)
+        {
+            Equipe e = c.Equipe.First(x => x.IdEquipe == id);
+
+            c.Equipe.Remove(e);
+
+            c.SaveChanges();
+
+            return LocalRedirect("~/Equipe/listar");
+
+        }
+
+        [Route("Editar/{id}")]
+        public IActionResult Editar(int id)
+        {
+            Equipe e = c.Equipe.First(x => x.IdEquipe == id);
+
+            ViewBag.Equipe = e;
+
+            return View("Edit");
+        }
+
+        [Route("Atualizar")]
+        public IActionResult Atualizar(IFormCollection form, Equipe e)
+        {
+
+            Equipe novaEquipe = new Equipe();
+
+            novaEquipe.Nome = e.Nome;
+
+            // upload da imagem da equipe nova (atualizado)
+
+            if (form.Files.Count > 0)
+            {
+                var file = form.Files[0];
+
+                var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
+
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+
+                var path = Path.Combine(folder, file.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                novaEquipe.Imagem = file.FileName;
+
+            }
+            else
+            {
+                novaEquipe.Imagem = "padrao.png";
+            }
+
+            Equipe equipe = c.Equipe.First(x => x.IdEquipe == e.IdEquipe);
+
+            equipe.Nome = novaEquipe.Nome;
+            equipe.Imagem = novaEquipe.Imagem;
+
+            c.Equipe.Update(equipe);
+
+            c.SaveChanges();
+
+            return LocalRedirect("~/Equipe/Listar");
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
